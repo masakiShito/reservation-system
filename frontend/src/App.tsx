6e0d92fb29c1f678f15957d9c92b49070c5f801d
masakiShito <happy.engineer.life@gmail.com>
2024-10-19 08:36:55 +0900

    フロント側のログイン・サインアップ機能を作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 // src/App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Reservations from './pages/Reservations';
-
 const App: React.FC = () => {
-  return (
-      <div className="App">
-        <Reservations />
-      </div>
-  );
+    const isAuthenticated = !!localStorage.getItem('jwt');
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/reservations" element={isAuthenticated ? <Reservations /> : <Navigate to="/login" />} />
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
